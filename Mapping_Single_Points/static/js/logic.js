90f2d7b8c788ab14834ea67be922715a0a49009c
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-14 14:13:35 -0500

    As per OSU Module 13.4.1.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -22,7 +22,27 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // mapbox / satellite - v9
 // mapbox / satellite - streets - v11
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Adding a circle instead with radius 100m.
+// 
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: 'red',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+// See the difference between circleMarker()
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: 'red',
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
