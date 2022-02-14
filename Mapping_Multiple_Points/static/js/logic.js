dd54fe369bb95598a4e11225f88d9d06418d1545
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-14 15:26:46 -0500

    As per OSU Module 13.4.2.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -23,42 +23,15 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // mapbox / satellite - streets - v11
 // We create the tile layer that will be the background of our map.
 
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-},
-{
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-},
-{
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-},
-{
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-},
-{
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-}
-];
+// Get data from cities.js
+let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function (city) {
-    console.log(city.state)
+cityData.forEach(function (city) {
+    console.log(city)
+    L.circleMarker(city.location, {radius: (city.population/100000), color: 'orange', lineweight: 4})
+        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+        .addTo(map);
 });
 
 
