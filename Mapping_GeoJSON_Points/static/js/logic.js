af3ad4cd091087520df34dacb8ee97dff94f8ba9
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-16 18:00:27 -0500

    As per OSU Module 13.5.4 Add Multiple Maps.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 // let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 // let sanFranAirport =
@@ -80,14 +80,39 @@ let map = L.map('mapid').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
 
 
+// Streets Map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+// Dark Map
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
 
 // Accessing data from GitHub account.
 let airportData = "https://raw.githubusercontent.com/tkirk70/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
@@ -103,3 +128,7 @@ d3.json(airportData).then(function (data) {
             layer.bindPopup("<h3>Airport Code: " + feature.properties.faa + "</h3> <hr> <h3>Airport Name: " + feature.properties.name + "</h3");}
         }).addTo(map);
 });
+
+// The onEachFeature option is a function that gets called on each feature before adding it
+// to a GeoJSON layer. A common reason to use this option is to
+// attach a popup to features when they are clicked.
