adb384390f2df4f500591d09cbafa956a991a585
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2022-02-17 11:40:07 -0500

    As per OSU Module 13.6.3.

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -98,16 +98,15 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
-
-
-
         // We turn each feature into a circleMarker on the map.
-
         pointToLayer: function (feature, latlng) {
             console.log(data);
             return L.circleMarker(latlng);
         },
         // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " +feature.properties.place);
+        }
     }).addTo(map);
 });
